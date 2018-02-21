022331f734bc9ab60486434ef481c702b94316b1
John Smith <john.texastech.smith@gmail.com>
2018-02-21 17:16:38 +0000

    Make user.owner_id nullable
    made user.owner_id nullable.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
-            $table->integer('owner_id');
+            $table->integer('owner_id')->nullable();
             $table->timestamps();
         });
     }
